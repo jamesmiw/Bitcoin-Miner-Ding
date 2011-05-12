1735135d2aea24afb4b0125cece7a17ca64ba6d1
unfinishe <jamesmiw@gmail.com>
2011-05-12 02:10:01 -0700

    First and hopefully only commit

---
+++ ding.cpp
@@ -0,0 +1,59 @@
+#include <windows.h>
+#include <mmsystem.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "Checking for output..." << endl;
+	cout << "Press spacebar to quit" << endl;
+	
+	string last, current;
+	string line;
+	
+	string check;
+	
+	if (argc > 1) check = argv[1];
+	else check = "accepted";
+	
+	last = "";
+	current = "";
+	
+	ifstream file;
+	
+	file.open("output.txt");
+	if (file.is_open())
+	{
+		while (file.good())
+		{
+			getline (file,line);
+			last += "\r\n" + line ;
+		}	
+		file.close();
+	}
+	
+	while (GetAsyncKeyState(VK_ESCAPE) == 0) {
+		Sleep(2000);
+		
+		current = "";
+		file.open("output.txt");		
+		if (file.is_open())
+		{
+			while (file.good())
+			{
+				getline (file,line);
+				current += "\r\n" + line;
+			}	
+			file.close();
+		}
+		if (current.substr(last.length()).rfind(argv[1]) != string::npos) {
+			PlaySound(TEXT("ding.wav"), NULL, SND_FILENAME);						
+		}
+		cout << current.substr(last.length());
+		last = current;			
+	}
+		
+	return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,19 @@
+Bitcoin Miner "Ding" for Windows
+by unfinishe
+
+How to use:
+
+1. In one cmd window, run your miner from the command-line, except add
+	> output.txt 2>&1
+to the end.
+
+[miner] [options] > output.txt 2>&1
+
+2. In another cmd window, run ding.exe followed by a word to look for.
+ding.exe [word]
+
+Here, ding.exe will show the output from your miner, 
+and if it comes across [word] in the output, it will 
+play the sound "ding.wav".
+
+Enjoy!
